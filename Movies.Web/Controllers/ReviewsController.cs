52e690a426ade430d71f12e945225ce23c2cd0f6
Tyrone Davison <t.p.davison@tees.ac.uk>
2022-10-23 14:10:54 +0100

    Add placeholder reviews controller

---
+++ Movies.Web/Controllers/ReviewsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Movies.Web.Controllers;
+
+public class ReviewsController : Controller
+{
+    // GET: /reviews/
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    // GET: /reviews/details/{id}
+    public IActionResult Details(int id)
+    {
+        return View();
+    }
+}
